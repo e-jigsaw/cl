26e7a856f21924075affad638c95d1b68efe03b0
jigsaw <m@jgs.me>
2024-08-14 15:19:32 +0000

    context as thread

--- index.ts
+++ index.ts
@@ -25,31 +25,59 @@ discord.on("messageCreate", async (message) => {
     await message.channel.send("pong");
   }
   if (message.channelId === Bun.env.DISCORD_CHANNEL_ID) {
-    let answer = "";
-    const stream = client.messages
-      .stream({
-        model: "claude-3-5-sonnet-20240620",
-        max_tokens: 1024,
-        messages: [
-          {
-            role: "user",
-            content: message.content,
-          },
-        ],
-      })
-      .on("text", (str) => {
-        answer += str;
-      });
     const thread = await message.startThread({
       name: message.content,
     });
-    const res = await thread.send(answer.length === 0 ? "(考え中)" : answer);
-    const id = setInterval(async () => {
-      await res.edit(answer.length === 0 ? "(考え中)" : answer);
-    }, 5000);
-    await stream.finalMessage();
-    clearInterval(id);
-    await res.edit(answer.length === 0 ? "わからん！" : answer);
+    const res = await thread.send("(考え中)");
+    const assistantMessage = await client.messages.create({
+      model: "claude-3-5-sonnet-20240620",
+      max_tokens: 1024,
+      messages: [
+        {
+          role: "user",
+          content: message.content,
+        },
+      ],
+    });
+    if (assistantMessage.content[0].type === "text") {
+      await res.edit(assistantMessage.content[0].text);
+    } else {
+      await res.edit("わからん！");
+    }
+  }
+  if (message.channel.isThread()) {
+    if (message.channel.parentId === Bun.env.DISCORD_CHANNEL_ID) {
+      const res = await message.channel.send("(考え中)");
+      const [allMessages, startMessage] = await Promise.all([
+        message.channel.messages.fetch(),
+        message.channel.fetchStarterMessage(),
+      ]);
+      if (startMessage?.content) {
+        const messages = [
+          {
+            role: "user",
+            content: startMessage.content,
+          },
+          ...allMessages
+            .filter((msg) => msg.content.length > 0)
+            .reverse()
+            .map((msg) => ({
+              role: msg.author.bot ? "assistant" : "user",
+              content: msg.content,
+            })),
+        ] as Array<{ role: "user" | "assistant"; content: string }>;
+        const assistantMessage = await client.messages.create({
+          model: "claude-3-5-sonnet-20240620",
+          max_tokens: 1024,
+          messages,
+        });
+        if (assistantMessage.content[0].type === "text") {
+          res.edit(assistantMessage.content[0].text);
+        } else {
+          res.edit("わからん！");
+        }
+      }
+    }
   }
 });
 
